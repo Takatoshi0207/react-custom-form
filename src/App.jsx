589ac07880a78bf09daa366e0fb92b57dc63c1cc
Takatoshi0207 <t.kotani@m-okuchu.com>
2022-10-24 21:18:58 +0900

    handleChangeの設定

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,56 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const initialValues = { username: "", mailAddress: "", password: "" };
+  const [formValues, setFormValues] = useState(initialValues);
+
+  const handleChange = (e) => {
+    // console.log(e.target.value)
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+    console.log(formValues);
+  };
 
   return (
     <div className="formContainer">
       <form>
-        <h1>ログインフォーム</h1><hr />
+        <h1>ログインフォーム</h1>
+        <hr />
         <div className="uiForm">
-          <div className="formField">
+          <div className="formField" onChange={handleChange}>
             <label>ユーザー名</label>
-            <input type="text" placeholder='ユーザー名' name='username' />
+            <input
+              type="text"
+              placeholder="ユーザー名"
+              name="username"
+              value={formValues.username}
+              onChange={(e) => handleChange(e)}
+            />
           </div>
           <div className="formField">
             <label>メールアドレス</label>
-            <input type="text" placeholder='メールアドレス' name='mailAdress' />
+            <input
+              type="text"
+              placeholder="メールアドレス"
+              name="mailAddress"
+              onChange={(e) => handleChange(e)}
+            />
           </div>
           <div className="formField">
             <label>パスワード</label>
-            <input type="text" placeholder='パスワード' name='password' />
+            <input
+              type="text"
+              placeholder="パスワード"
+              name="password"
+              onChange={(e) => handleChange(e)}
+            />
           </div>
           <button className="submitButton">ログイン</button>
         </div>
       </form>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
